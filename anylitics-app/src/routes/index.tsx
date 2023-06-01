32794f531fcab2f080ad8a7f14fbd48de35e4e80
Rupam Kairi <rupamkairi@hotmail.com>
2023-06-01 23:44:01 +0530

    drizzle orm with NEONdb on Solid Start

--- anylitics-app/src/routes/index.tsx
+++ anylitics-app/src/routes/index.tsx
@@ -1,12 +1,37 @@
-import { Title } from "solid-start";
+import { Title, createRouteData, useRouteData } from "solid-start";
 import Counter from "~/components/Counter";
+import ws from "ws";
+import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import { drizzle } from "drizzle-orm/neon-serverless";
+import { neonConfig, Pool } from "@neondatabase/serverless";
+
+neonConfig.webSocketConstructor = ws;
+
+const users = pgTable("users", {
+  id: serial("id").primaryKey(),
+  name: text("name"),
+});
+
+export function routeData() {
+  return createRouteData(async () => {
+    const pool = new Pool({
+      connectionString: import.meta.env.VITE_NEON_DBURI,
+    });
+    const db = drizzle(pool);
+    const result = await db.select().from(users);
+    return result;
+  });
+}
 
 export default function Home() {
+  const users = useRouteData<typeof routeData>();
+
   return (
     <main>
       <Title>Hello World</Title>
       <h1>Hello world!</h1>
       <Counter />
+      <pre>{JSON.stringify(users())}</pre>
       <p>
         Visit{" "}
         <a href="https://start.solidjs.com" target="_blank">
--- anylitics-app/vite.config.ts
+++ anylitics-app/vite.config.ts
@@ -2,5 +2,8 @@ import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  server: {
+    host: true,
+  },
   plugins: [solid()],
 });
