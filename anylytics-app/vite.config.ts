31e559f37aa4995b2a59cfa626c0874f2666b90c
Rupam Kairi <rupamkairi@hotmail.com>
2023-10-15 20:04:31 +0530

    Vercel Deployment fix

--- anylytics-app/vite.config.ts
+++ anylytics-app/vite.config.ts
@@ -1,9 +1,13 @@
 import path from "path";
-import solid from "solid-start/vite";
+import solidstart from "solid-start/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [
+    solidstart({
+      ssr: false,
+    }),
+  ],
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "./src"),
